dc57e1a5200bf4e22b1d93eae0eb1b7164f1585b
ChrystelMarcelo <chrystel_anne_marcelo@dlsu.edu.ph>
2024-07-31 04:21:05 +0800

    Update Standard.java

--- Standard.java
+++ Standard.java
@@ -8,11 +8,21 @@ public class Standard extends Room {
         super(roomName, roomPrice);
     }
 
+    /**
+     * Gets the price of the room.
+     *
+     * @return the price of the room
+     */
     @Override
     public double getPrice() {
         return getBaseRate();
     }
 
+    /**
+     * Gets the type of the room.
+     *
+     * @return the type of the room
+     */
     @Override
     public String getRoomType(){
         return "Standard";
