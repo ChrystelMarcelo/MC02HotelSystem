79f3c794e3a350ee70131f0f1fbfc08a8668baa3
ChrystelMarcelo <chrystel_anne_marcelo@dlsu.edu.ph>
2024-07-31 04:19:07 +0800

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,142 +1,97 @@
+import java.util.ArrayList;
+
+
 /**
- * Represents a reservation made by a guest for a room in a hotel.
+ * The Room Class represents a room in a hotel
  */
-public class Reservation {
-    private String guestName;
-    private int checkInDate;
-    private int checkOutDate;
-    private Room room;
-    private double totalPrice;
-    private String discountCode;
-    private Hotel hotel;
+public abstract class Room {
+    private String roomName;
+    private double baseRate;
+    private boolean available;
+
 
     /**
-     * Constructs a new Reservation object.
-     * @param guestName    The name of the guest making the reservation.
-     * @param checkInDate  The check-in date of the reservation
-     * @param checkOutDate The check-out date of the reservation
-     * @param room         The Room object reserved by the guest.
+     * Constructs a Room object with a given room name
+     * @param roomName
      */
-    Reservation(Hotel hotel, String guestName, int checkInDate, int checkOutDate, Room room, String discountCode){
-        this.guestName = guestName;
-        this.checkInDate = checkInDate;
-        this.checkOutDate = checkOutDate;
-        this.room = room;
-        this.discountCode = discountCode;
-        this.hotel = hotel;
-        room.setIsAvailable(false);
-        this.totalPrice = calculateTotalPrice();
-    }
-
-    /**
-     * Returns the total price of the reservation.
-     * @return The total price of the reservation.
-     */
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    /**
-     * Returns the check-in date of the reservation.
-     * @return The check-in date of the reservation.
-     */
-    public int getCheckInDate() {
-        return checkInDate;
-    }
-
-    /**
-     * Returns the check-out date of the reservation.
-     * @return The check-out date of the reservation.
-     */
-    public int getCheckOutDate() {
-        return checkOutDate;
+    Room(String roomName, double baseRate){
+        this.roomName = roomName;
+        this.baseRate = baseRate;
+        this.available = true;
     }
 
 
     /**
-     * Returns the name of the guest who made the reservation.
-     * @return The name of the guest.
+     * Returns the name of the room
+     * @return the name of the room
      */
-    public String getGuestName() {
-        return guestName;
-    }
-
-    /**
-     * Returns the Room object associated with this reservation.
-     * @return The Room object reserved by the guest.
-     */
-    public Room getRoom() {
-        return room;
+    public String getRoomName(){
+        return this.roomName;
     }
 
 
     /**
-     * Calculates the total price for the reservation.
+     * Sets the base price per night
+     * @param roomPrice the base price of the room
+     */
+    public void setRoomPrice(double roomPrice){
+        baseRate = roomPrice;
+    }
+
+
+    /**
+     * Gets the price of the room.
      *
-     * @return the total price for the reservation
+     * @return the price of the room
      */
-    public double calculateTotalPrice(){
-        double totalPrice = 0.0;
-        for (int day = checkInDate; day < checkOutDate; day++) {
-            totalPrice = totalPrice + hotel.getPriceForDate(day, room);
-        }
-        totalPrice = applyDiscount(totalPrice, checkOutDate - checkInDate);
-        return totalPrice;
-    }
+    public abstract double getPrice();
 
     /**
-     * Recalculates and updates the total price for the reservation.
-     */
-    public void recalculateTotalPrice(){
-        this.totalPrice = calculateTotalPrice();
-    }
-
-
-    /**
-     * Applies a discount to the total price based on the discount code.
+     * Gets the type of the room.
      *
-     * @param totalPrice the original total price
-     * @param numberOfDays the number of days for the reservation
-     * @return the total price after applying the discount
+     * @return the type of the room
      */
-    private double applyDiscount(double totalPrice, int numberOfDays){
-        switch(discountCode){
-            case "I_WORK_HERE":
-                return totalPrice - (totalPrice* 0.1);
-            case "STAY4_GET1":
-                if(numberOfDays >= 5){
-                    return totalPrice - (totalPrice/numberOfDays);
-                }
-                break;
-            case "PAYDAY":
-                if(checkPayday(checkInDate, checkOutDate)){
-                    return totalPrice - (totalPrice * 0.07);
-                }
-                break;
-            default:
-                return totalPrice;
-        }
+    public abstract String getRoomType();
 
-        return totalPrice;
+    /**
+     * Returns the base price per night
+     * @return the base price per night
+     */
+    public double getBaseRate(){
+        return baseRate;
     }
 
     /**
-     * Checks if date is applicable to discount PAYDAY
-     *
-     * @param checkInDate the check-in date
-     * @param checkOutDate the check-out date
-     * @return true if reservation date applies to discount PAYDAY, otherwise false
+     * Sets the availability of a room
+     * @param isBooked availability of a room
      */
-    private boolean checkPayday(int checkInDate, int checkOutDate){
-        int i;
+    public void setIsAvailable(boolean isBooked){
+        this.available = isBooked;
+    }
 
-        for(i = checkInDate; i < checkOutDate; i++){
-            if (i == 15 || i == 30){
-                return true;
+    /**
+     * Returns availability of a room
+     * @return availability of a room
+     */
+    public boolean getIsAvailable() {
+        return available;
+    }
+
+
+    /**
+     * Returns if room is available on a date in a month
+     * @param date the day of a month
+     * @param reservations list of reservations
+     * @return false if date is booked, true if otherwise
+     */
+    public boolean isAvailableMonth(int date, ArrayList<Reservation> reservations) {
+        for (Reservation res : reservations) {
+            if (date >= res.getCheckInDate() && date <= res.getCheckOutDate()) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
-
 }
+
